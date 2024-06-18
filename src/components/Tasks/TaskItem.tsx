a84c53b4b35fc4f8b6392cf143fe9d48ab261c4a
valerialunaorla <valeria.luna@orla.tech>
2024-06-18 17:11:53 -0300

    feat: cria os componentes de TasksList e TaskItem

---
+++ src/components/Tasks/TaskItem.tsx
@@ -0,0 +1,47 @@
+import { Image, StyleSheet, Text, TouchableOpacity } from 'react-native';
+
+export type Props = {
+  isSelected?: boolean;
+  label: string;
+  status?: 'READY' | 'IN_PROGRESS' | 'FINISHED';
+};
+
+const tinyCheckImage = require('../../../assets/tiny-check.png');
+
+export function TaskItem({ label, status = 'READY', isSelected = false }: Props) {
+  return (
+    <TouchableOpacity
+      style={[styles.container, isSelected && styles.containerSelected]}
+      onPress={() => {}}
+    >
+      <Text style={styles.label}>{label}</Text>
+      {status === 'IN_PROGRESS' && <Text style={styles.statusText}>In progress</Text>}
+      {status === 'FINISHED' && <Image source={tinyCheckImage} />}
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: 'rgba(0, 0, 0, 0.1)',
+    borderRadius: 4,
+    borderLeftWidth: 5,
+    borderLeftColor: 'transparent',
+    padding: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginBottom: 10,
+  },
+  containerSelected: {
+    borderLeftColor: '#fff',
+  },
+  label: {
+    color: '#fff',
+    fontSize: 14,
+  },
+  statusText: {
+    color: '#fff',
+    fontSize: 14,
+    fontWeight: 'bold',
+  },
+});
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,17 +1,33 @@
-import { Image, SafeAreaView, StyleSheet, Text, View } from "react-native";
-import { Theme } from "../themes";
-import { FabButton, NewTaskModal, NoTasksCards, Timer } from "../components";
-import { useState } from "react";
+import { Image, SafeAreaView, StyleSheet, View } from 'react-native';
+import { Theme } from '../themes';
+import { FabButton, NewTaskModal, NoTasksCards, TasksList, Timer } from '../components';
+import { useState } from 'react';
 
-const logo = require("../../assets/logo.png");
+const logoImage = require('../../assets/logo.png');
 
 export function HomeScreen() {
   const [isNewTaskModalVisible, setIsNewTaskModalVisible] = useState(false);
   return (
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.container}>
-        <Image source={logo} style={styles.logo} resizeMode="contain" />
-        {false ? <NoTasksCards /> : <Timer />}
+        <Image source={logoImage} style={styles.logo} resizeMode='contain' />
+        {false ? (
+          <NoTasksCards />
+        ) : (
+          <>
+            <View style={styles.timerContainer}>
+              <Timer />
+            </View>
+
+            <TasksList
+              data={[
+                { isSelected: true, label: 'Create a frontend project', status: 'IN_PROGRESS' },
+                { isSelected: false, label: 'Create a frontend project', status: 'READY' },
+                { isSelected: false, label: 'Create a frontend project', status: 'READY' },
+              ]}
+            />
+          </>
+        )}
 
         <FabButton onPress={() => setIsNewTaskModalVisible(true)} />
         <NewTaskModal
@@ -30,13 +46,14 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
-    paddingVertical: 40,
-    paddingHorizontal: 20,
   },
   logo: {
     width: 227,
     height: 46,
-    alignSelf: "center",
-    marginBottom: 20,
+    alignSelf: 'center',
+    marginVertical: 30,
+  },
+  timerContainer: {
+    paddingHorizontal: 20,
   },
 });
