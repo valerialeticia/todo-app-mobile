5d8d46d3c1d4d19fddd921bab3576c492a0c59de
valerialunaorla <valeria.luna@orla.tech>
2024-06-20 16:09:26 -0300

    feat: adiciona prop selectedIndex e onPress no componente TasksList

--- src/components/Tasks/TasksList.tsx
+++ src/components/Tasks/TasksList.tsx
@@ -2,15 +2,19 @@ import { FlatList, StyleSheet } from 'react-native';
 import { TaskItem, Props as TaskItemProps } from './TaskItem';
 
 type Props = {
-  data: TaskItemProps[];
+  data: Omit<TaskItemProps, 'onPress'>[];
+  selectedIndex?: number;
+  onPress: (index: number) => void;
 };
 
-export function TasksList({ data }: Props) {
+export function TasksList({ data, selectedIndex, onPress }: Props) {
   return (
     <FlatList
       data={data}
       contentContainerStyle={styles.container}
-      renderItem={({ item }) => <TaskItem {...item} />}
+      renderItem={({ item, index }) => (
+        <TaskItem {...item} isSelected={index === selectedIndex} onPress={() => onPress(index)} />
+      )}
     />
   );
 }
