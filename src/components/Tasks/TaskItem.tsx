9a293d6d6d1114d6076e9eaf176847f65c455471
valerialunaorla <valeria.luna@orla.tech>
2024-06-20 16:08:58 -0300

    feat: adiciona prop onPress no componente TaskItem

--- src/components/Tasks/TaskItem.tsx
+++ src/components/Tasks/TaskItem.tsx
@@ -4,15 +4,16 @@ export type Props = {
   isSelected?: boolean;
   label: string;
   status?: 'READY' | 'IN_PROGRESS' | 'FINISHED';
+  onPress: () => void;
 };
 
 const tinyCheckImage = require('../../../assets/tiny-check.png');
 
-export function TaskItem({ label, status = 'READY', isSelected = false }: Props) {
+export function TaskItem({ label, status = 'READY', isSelected = false, onPress }: Props) {
   return (
     <TouchableOpacity
       style={[styles.container, isSelected && styles.containerSelected]}
-      onPress={() => {}}
+      onPress={onPress}
     >
       <Text style={styles.label}>{label}</Text>
       {status === 'IN_PROGRESS' && <Text style={styles.statusText}>In progress</Text>}
